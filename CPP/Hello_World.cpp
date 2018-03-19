2576045b5fef84539dd9ea41c7b21ebeda9885bf
Kyle Cotton <kylecottonkc@gmail.com>
2018-03-19 19:51:51 +0000

    Added Java Directory

--- CPP/Hello_World.cpp
+++ CPP/Hello_World.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World\n";
-	return 0;
+  cout << "Hello World\n";
+  cout << "Good Program";
+  return 0;
 }
 
 
---
+++ Python/test.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+for i in range(0,10):
+    print("Hello, {}".format(i))
+
+
