e38938fec308024d1e2ac8e94c3ec9f87c738cc1
Kyle Cotton <kylecottonkc@gmail.com>
2018-03-20 17:37:51 +0000

    Added the new loop feature.

--- Java/Display.java
+++ Java/Display.java
@@ -2,7 +2,8 @@ public class Display {
     public static void main(String args[]) {
 	System.out.println("Hello World!");
 	System.out.println("First Java Program");
-
-	
+	for(int i=0; i<10; i++){
+	    System.out.println("Loop Number: " + i);
+         }
     }
 }
