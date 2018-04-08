4e5c1781433e337b8b5709ebffeeb53abaa782af
Kyle Cotton <kylecottonkc@gmail.com>
2018-04-08 19:41:01 +0100

    Added basic printing.

--- Java/Car.java
+++ Java/Car.java
@@ -5,7 +5,10 @@ public class Car {
 	this.numberOfWheels = numberOfWheels;
     }
 
-
+    public int getNumberOfWheels() {
+	return numberOfWheels;
+    }
+    
     public void startEngine() {
 	System.out.println("Start your engine which has " + numberOfWheels + " wheels");
     }
@@ -13,6 +16,11 @@ public class Car {
     public static void main(String[] args) {
 	Car sparky = new Car(3);
 	sparky.startEngine();
+
+
+        System.out.println("Testing this feature");
+	
+	System.out.println(sparky.getNumberOfWheels());
     }
 }
 
